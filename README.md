df7cdd2436820c5c0984e5741b2e1403b03e9f72
unknown <rafaelcerq91@gmail.com>
2022-03-22 00:08:03 -0300

    Resposta e ações para seus comandos

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # AssistenteVirtual
 Assistente Virtual com Python e AI
 
+Versão Python: 3.6.*
 
 Instalações:
 - Configurar ambiente virtual (virtualenv venv)
-- Instalar SpeechRecognition
-- Instalar PyAudio
+- Instalar SpeechRecognition ( pip install SpeechRecognition )
+- Instalar PyAudio ( pip install pyaudio )
+- Instalar pyttsx3 ( pip install pyttsx3 )
--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import speech_recognition as sr
 import re
+import pyttsx3
 
 nome = ""
 
@@ -9,6 +10,8 @@ while(True):
 
     with sr.Microphone() as source:
 
+        engine = pyttsx3.init()
+        engine.setProperty('voice', "com.apple.speech.synthesis.voice.luciana")
         mic.adjust_for_ambient_noise(source)
 
         print("Vamos começar, fale algo...")
@@ -19,12 +22,16 @@ while(True):
             frase = mic.recognize_google(audio, language='pt-BR')
 
             if (re.search(r'\b' + "ajudar" + r'\b', format(frase))):
+                engine.say("Ajuda");
+                engine.runAndWait()
                 print("Algo relacionado a ajuda.")
 
             elif (re.search(r'\b' + "Meu nome é " + r'\b', format(frase))):
                 t = re.search('Meu nome é (.*)', format(frase))
                 nome = t.group(1)
                 print("Seu nome é " + nome)
+                engine.say("Nome falado foi " + nome);
+                engine.runAndWait()
 
             print("Voce falou: " + frase)
 
