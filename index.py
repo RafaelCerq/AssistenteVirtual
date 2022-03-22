b39c4c8bea48153b171c9e4e805bab9b28d34080
unknown <rafaelcerq91@gmail.com>
2022-03-21 23:57:47 -0300

    Interpretando comando Avançado

--- index.py
+++ index.py
@@ -1,25 +1,32 @@
 import speech_recognition as sr
 import re
 
+nome = ""
+
 while(True):
 
     mic = sr.Recognizer()
 
     with sr.Microphone() as source:
+
         mic.adjust_for_ambient_noise(source)
 
-        print("Vamos começar, fale algo..")
+        print("Vamos começar, fale algo...")
 
         audio = mic.listen(source)
 
         try:
-            frase = mic.recognize_google(audio, language='pt-br')
+            frase = mic.recognize_google(audio, language='pt-BR')
 
             if (re.search(r'\b' + "ajudar" + r'\b', format(frase))):
-                print("algo relacionado a ajuda.")
+                print("Algo relacionado a ajuda.")
 
+            elif (re.search(r'\b' + "Meu nome é " + r'\b', format(frase))):
+                t = re.search('Meu nome é (.*)', format(frase))
+                nome = t.group(1)
+                print("Seu nome é " + nome)
 
-            print('Você falou: ' + frase)
+            print("Voce falou: " + frase)
 
         except sr.UnknownValueError:
             print("Algo deu errado")
